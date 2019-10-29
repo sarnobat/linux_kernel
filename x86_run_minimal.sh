386a06ceaeaf05b72766444ad487abc7342edf13
sarnobat <ss401533@gmail.com>
2019-10-28 23:26:24 -0700

    Update x86_run_minimal.sh

--- x86_run_minimal.sh
+++ x86_run_minimal.sh
@@ -1,4 +1,4 @@
-/media/sarnobat/unmirrored/trash/buildroot-arm.2019.02.6.cheat/linux-kernel-module-cheat/out/qemu/default/opt/x86_64-softmmu/qemu-system-x86_64 \
+qemu-system-x86_64 \
   -kernel /media/sarnobat/unmirrored/trash/buildroot-arm.2019.02.6.cheat/linux-kernel-module-cheat/out/linux/default/x86_64/arch/x86/boot/bzImage \
   -append 'root=/dev/vda  console=ttyS0' \
   -nographic \
