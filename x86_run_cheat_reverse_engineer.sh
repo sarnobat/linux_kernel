2f912b090886d9fc5928431999620895b50670ef
sarnobat <ss401533@gmail.com>
2019-11-11 14:09:11 -0800

    Update x86_run_cheat_reverse_engineer.sh

--- x86_run_cheat_reverse_engineer.sh
+++ x86_run_cheat_reverse_engineer.sh
@@ -1,7 +1,7 @@
-cd /media/sarnobat/unmirrored/trash/buildroot-arm.2019.02.6.cheat/linux-kernel-module-cheat/ && \
-./build-buildroot \
-	  --no-show-time \
-	  ;
+#cd /media/sarnobat/unmirrored/trash/buildroot-arm.2019.02.6.cheat/linux-kernel-module-cheat/ && \
+#./build-buildroot \
+#	  --no-show-time \
+#	  ;
 
 cd /media/sarnobat/unmirrored/trash/buildroot-arm.2019.02.6.cheat/linux-kernel-module-cheat/submodules/buildroot && \
 	  make \
@@ -10,7 +10,7 @@ cd /media/sarnobat/unmirrored/trash/buildroot-arm.2019.02.6.cheat/linux-kernel-m
 	        qemu_x86_64_defconfig \
 		;
 
-
+cd /media/sarnobat/unmirrored/trash/buildroot-arm.2019.02.6.cheat/linux-kernel-module-cheat/submodules/buildroot && \
 make O=/media/sarnobat/unmirrored/trash/buildroot-arm.2019.02.6.cheat/linux-kernel-module-cheat/out/buildroot/build/default/x86_64 BR2_EXTERNAL=../../buildroot_packages/kernel_modules:../../buildroot_packages/lkmc_many_files:../../buildroot_packages/parsec_benchmark:../../buildroot_packages/sample_package qemu_x86_64_defconfig
 
 
@@ -19,14 +19,6 @@ cat \
 	    '>>' \
 	      /media/sarnobat/unmirrored/trash/buildroot-arm.2019.02.6.cheat/linux-kernel-module-cheat/out/buildroot/build/default/x86_64/.config \
 	      ;
-
-
-cd /media/sarnobat/unmirrored/trash/buildroot-arm.2019.02.6.cheat/linux-kernel-module-cheat/submodules/buildroot && \
-	  make \
-	    O=/media/sarnobat/unmirrored/trash/buildroot-arm.2019.02.6.cheat/linux-kernel-module-cheat/out/buildroot/build/default/x86_64 \
-	      olddefconfig \
-	      ;
-
 
 cd /media/sarnobat/unmirrored/trash/buildroot-arm.2019.02.6.cheat/linux-kernel-module-cheat/submodules/buildroot && \
 	  make \
@@ -39,5 +31,5 @@ unset PERL_MM_OPT && PATH=/bin/:/usr/local/bin:/usr/bin:/sbin make \
 	'LKMC_PARSEC_BENCHMARK_SRCDIR="/media/sarnobat/unmirrored/trash/buildroot-arm.2019.02.6.cheat/linux-kernel-module-cheat/submodules/parsec-benchmark"' \
 	O=/media/sarnobat/unmirrored/trash/buildroot-arm.2019.02.6.cheat/linux-kernel-module-cheat/out/buildroot/build/default/x86_64 \
 	V=0 \
-	all \
+	busybox-rebuild linux-rebuild all \
 ;
